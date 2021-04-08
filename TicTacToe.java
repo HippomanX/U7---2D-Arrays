1acb7dbf347ea6f676ce84f85f7140989615ad69
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-04-07 20:02:26 -0700

    tictactoe edits

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,17 +1,164 @@
-public TicTacToe(int numRows, int numCols){
-   /*Setup a new empty TicTacToe board with
-   numRows rows and numCols columns.
-   Input:
-      int numRows: the number of rows on the board
-      int numCols: the number of columns on the board
-   Output:
-      this: a new empty Tic Tac Toe board, with each box
-      filled with an empty space
-   Ex.
-   TicTacToe ttt = new TicTacToe(3, 3);
-   ttt.print()
-   [ ] [ ] [ ]
-   [ ] [ ] [ ]
-   [ ] [ ] [ ]
-   */
+public class TicTacToe{
+   private String[][] board; //represents the game board
+   //private int Rows;
+   //private int Cols;
+   public TicTacToe(int numRows, int numCols){
+      /*Setup a new empty TicTacToe board with
+      numRows rows and numCols columns.
+      Input:
+         int numRows: the number of rows on the board
+         int numCols: the number of columns on the board
+      Output:
+         this: a new empty Tic Tac Toe board
+      Ex.
+      TicTacToe ttt = new TicTacToe(3, 3);
+      ttt.print()
+      [ ] [ ] [ ]
+      [ ] [ ] [ ]
+      [ ] [ ] [ ]
+      */
+      board = new String[numRows][numCols];
+      for (int x = 0; x < numRows; x++){
+         for (int y = 0; y < numCols; y++){
+            this.board[x][y] = " ";
+         }
+      }
+      
+      
+      //Cols = numCols;
+   }
+   
+   public void print(){
+      /*Prints out a representation of this board to StdOut
+      Input:
+         this: a TicTacToe object
+      Output:
+         StdOut: prints out a representation of this board
+      Ex.
+      TicTacToe ttt = new TicTacToe(3, 3);
+      ...some moves later...
+      ttt.print()
+      [X] [ ] [O]
+      [O] [X] [X]
+      [ ] [ ] [x]
+      */
+      for (int i = 0; i < this.board.length; i++){
+         for(int j = 0; j < this.board[i].length; j++){
+            StdOut.print(" [" + this.board[i][j] + "]");
+         }
+         StdOut.println();
+      }
+   }
+   public void setX(int row, int col){
+      /*Set the square at (row, col) to "X"
+      Input:
+         this: a TicTacToe object
+         int row: the row coordinate
+         int col: the column coordinate
+      Output: none
+      Side Effects: the square at (row, col) is now "X"
+      Ex.
+      TicTacToe ttt = new TicTacToe(3, 3);
+      ttt.setX(0, 2);
+      ttt.print()
+      [ ] [ ] [X]
+      [ ] [ ] [ ]
+      [ ] [ ] [ ]
+      */
+      this.board[row][col] = "X";
+   }
+   public void setO(int row, int col){
+      /*Set the square at (row, col) to "O"
+      Input:
+         this: a TicTacToe object
+         int row: the row coordinate
+         int col: the column coordinate
+      Output: none
+      Side Effects: the square at (row, col) is now "O"
+      Ex.
+      TicTacToe ttt = new TicTacToe(3, 3);
+      ttt.setO(2, 0);
+      ttt.print()
+      [ ] [ ] [ ]
+      [ ] [ ] [ ]
+      [O] [ ] [ ]
+      */
+      this.board[row][col] = "O";
+   }
+   public String getSquare(int row, int col){
+      /*Get the value at (row, col)
+      Input:
+         this: a TicTacToe object
+         int row: the row coordinate
+         int col: the column coordinate
+      Output:
+         return: the value at (row, col)
+      Ex.
+      TicTacToe ttt = new TicTacToe(3, 3);
+      ttt.setX(0, 2);
+      ttt.setO(2, 0);
+      ttt.print()
+      [ ] [ ] [X]
+      [ ] [ ] [ ]
+      [O] [ ] [ ]
+      ttt.getSquare(0, 2) -> X
+      ttt.getSquare(2, 0) -> O
+      ttt.getSquare(1, 1) -> " " (quotes just to show space)
+      */
+      return this.board[row][col];
+   }
+   public boolean isEmpty(int row, int col){
+      /*Check if the square at (row, col) is empty
+      Input:
+         this: a TicTacToe object
+         int row: the row coordinate
+         int col: the column coordinate
+      Output:
+         return: whether or not the square at
+                 (row, col) is empty
+      Ex.
+      TicTacToe ttt = new TicTacToe(3, 3);
+      ttt.setX(0, 2);
+      ttt.setO(2, 0);
+      ttt.print()
+      [ ] [ ] [X]
+      [ ] [ ] [ ]
+      [O] [ ] [ ]
+      ttt.isEmpty(0, 2) -> false
+      ttt.isEmpty(1, 1) -> true
+      ttt.isEmpty(2, 0) -> false
+      */
+      if (this.board[row][col].equals("X") || this.board[row][col].equals("O")) return false;
+      else return true;
+   }
+   public int numEmpty(){
+      /*Count the number of empty squares left on the board
+      Input:
+         this: a TicTacToe object
+      Output:
+         return: the number of empty squares left on the board
+      Ex.
+      TicTacToe ttt = new TicTacToe(3, 3);
+      ttt.numEmpty() -> 9
+      ttt.setX(0, 0);
+      ttt.numEmpty() -> 8
+      ttt.setO(1, 1);
+      ttt.numEmpty() -> 7
+      */
+      int emptyCounter = 0;
+      for (int x = 0; x < this.board.length; x++){
+         for (int y = 0; y < this.board[x].length; y++){
+            if (this.board[x][y].equals(" ")){
+               emptyCounter++;
+            }
+         }
+      }
+      return emptyCounter;
+   }      
+
+
+
+
+
+   
 }
---
+++ TicTacToeDriver.java
@@ -0,0 +1,157 @@
+public class TicTacToeDriver{
+   private static TicTacToe gameBoard;
+   
+   //setup the game according to the user's instructions
+   public static void setupGame(){
+      System.out.println("How many rows would you like on the game board?");
+      int numRows = StdIn.readInt();
+      System.out.println("How many columns would you like on the game board?");
+      int numCols = StdIn.readInt();
+      //setup the game board
+      gameBoard = new TicTacToe(numRows, numCols);
+   }
+   //check whether board has 3 Os in a row in column col
+   /*public boolean threeVerticalOs(int col){
+      if (gameBoard.board[0][col].equals("O")){
+         if (gameBoard.board[1][col].equals("O")){
+            if (gameBoard.board[2][col].equals("O")){
+               return true;
+            }
+         }
+      }
+      else return false;
+   }
+   //check whether board has 3 Os in a row in any column
+   public boolean threeVerticalOs(){
+      for(int i = 0; i < gameBoard[i].length(); i++){
+         if (threeVerticalOs(i)) return true;
+         
+      }
+      return false;
+   }
+   //check whether board has 3 Os in a row in row row
+   public boolean threeHorizontalOs(int row){
+      if (gameBoard.board[row][0].equals("O")){
+         if (gameBoard.board[row][1].equals("O")){
+            if (gameBoard.board[row][2].equals("O")){
+               return true;
+            }
+         }
+      }
+      else return false;
+   }
+   //check whether board has 3 Os in a row in any row
+   public boolean threeHorizontalOs(){
+      for(int i = 0; i < gameBoard[i].length(); i++){
+         if (threeHorizontalOs(i)) return true;
+         
+      }
+      return false;
+   }
+   //check whether board has 3 Os in the diagonals
+   public boolean threeDiagonalOs(){
+   
+   }
+   //check if O has 3 in a row, any orientation
+   public boolean threeInARowO(){
+   
+   }
+   //check if O has 3 in a row, any orientation
+   public boolean threeInARowX(){
+
+   }*/
+   //run all the steps for the X player's turn
+   public static void xTurn(){
+      //ask the player where they would like to move
+      System.out.println("--------------X Player---------------");
+      gameBoard.print(); //print the board
+      System.out.println("Which row would you like to place on?");
+      int row = StdIn.readInt();
+      System.out.println("Which column would you like to place on?");
+      int col = StdIn.readInt();
+      //check if that space is free
+      if(gameBoard.isEmpty(row, col)){
+         gameBoard.setX(row, col);
+      }
+      else{//ask again
+         System.out.println("Please choose an empty space");
+         xTurn();
+      }
+   }
+   
+   //run all the steps for the O player's turn
+   public static void oTurn(){
+      
+      //ask the player where they would like to move
+      System.out.println("--------------O Player---------------");
+      gameBoard.print(); //print the board
+      System.out.println("Which row would you like to place on?");
+      int row = StdIn.readInt();
+      System.out.println("Which column would you like to place on?");
+      int col = StdIn.readInt();
+      //check if that space is free
+      if(gameBoard.isEmpty(row, col)){
+         gameBoard.setO(row, col);
+      }
+      else{//ask again
+         System.out.println("Please choose an empty space");
+         oTurn();
+      }
+   }
+   
+   //ask the players if the game is over. Return true if they say "y"
+   //false if they say "n", and ask again if neither
+   public static boolean askGameOver(){
+      //ask the player if the game is over
+      gameBoard.print(); //print out the board
+      if (gameBoard.numEmpty() == 0){
+         System.out.println("The game has ended. I hope you had fun playing!");
+         return true;
+      }
+      System.out.println("Is the game over?");
+      System.out.print("(y/n)");
+      String reply = StdIn.readString();
+      if(reply.equals("y")){
+         System.out.println("Good job winning!");
+         return true;
+         
+      }
+      else if (reply.equals("n")){
+         return false;
+      }
+      else{
+         System.out.println("Please type y or n");
+         askGameOver();
+      }
+      return false; //should never get here
+   }
+         
+   
+   
+   public static void main(String[] args){
+      setupGame(); //ask the user how to setup the game
+      
+      //do the first 3 rounds before asking if anyone one
+      
+      //Round 1
+      xTurn(); //X goes first
+      oTurn(); //O goes second
+      
+      //Round 2
+      xTurn(); //X goes first
+      oTurn(); //O goes second
+      
+      //Round 3
+      xTurn(); //X goes first
+      //check if anyone won yet
+      while(askGameOver() == false){//while the game is not over
+         oTurn();
+         if(askGameOver() == true){//if O won, break
+            break;
+         }
+         else{//otherwise do X's turn
+            xTurn();
+         }
+      }    
+   }
+}
